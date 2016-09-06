8f13985fa195bc944e8b57bd836f59c500a46090
Colin <Colin@DESKTOP-I6AF3ST>
2016-09-06 10:31:57 -0700

    initial commit, assign 2

---
+++ Assignment 2/CS422/CS422/PCQueue.cs
@@ -0,0 +1,44 @@
+﻿/* Colin Phillips
+ * CS 422 - Assignment 2
+ * Fall 2016 */
+
+using System;
+
+namespace CS422
+{
+  public class PCQueue
+  {
+    private PCQNode _head;
+    private PCQNode _tail;
+    private PCQNode _dummy;
+
+    public PCQueue()
+    {
+      _head = _tail = null;
+      _dummy = new PCQNode(-1);
+    }
+
+    public void Enqueue(int dataValue)
+    {
+      _tail.Next = new PCQNode(dataValue);
+      _tail = _tail.Next;
+    }
+
+    public bool Dequeue(ref int out_value)
+    {
+      
+    }
+  }
+
+  internal class PCQNode
+  {
+    public int Data { get; private set; }
+
+    public PCQNode Next { get; set; }
+
+    public PCQNode(int value)
+    {
+      Data = value;
+    }
+  }
+}
---
+++ Assignment 2/CS422/CS422/ThreadPoolSleepSorter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CS422
+{
+  public class ThreadPoolSleepSorter : IDisposable
+  {
+    private ManualResetEvent mre = new ManualResetEvent(false);
+    private TextWriter _writer;
+    private BlockingCollection<SleepSortTask> _taskCollection = new BlockingCollection<SleepSortTask>();
+    private List<Thread> _pool;
+
+    public ThreadPoolSleepSorter(TextWriter output, ushort threadCount)
+    {
+      if (output == null)
+      {
+        throw new ArgumentNullException("output");
+      }
+      _writer = output;
+
+      if (threadCount <= 0)
+      {
+        threadCount = 64;
+      }
+
+      _pool = new List<Thread>();
+
+      for (int i = 0; i < threadCount; i++)
+      {
+        mre.Wa
+        Thread thread = new Thread();
+        _pool.Add(thread.Start());
+      }
+    }
+
+    public void Sort(byte[] values)
+    {
+      
+    }
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  internal class SleepSortTask
+  {
+    public int Value { get; set; }
+
+    public TextWriter Writer { get; set; }
+
+    public static void Execute(TextWriter writer, int value)
+    {
+      writer.WriteLine(value);
+    }
+  }
+}
